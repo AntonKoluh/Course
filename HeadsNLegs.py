00849d6d3e6512397df9d22739fbd475a9c09aee
AntonKoluh <antonkoluh1@gmail.com>
2024-12-18 16:53:33 +0200

    Update HeadsNLegs.py

--- HeadsNLegs.py
+++ HeadsNLegs.py
@@ -3,7 +3,7 @@ Legs = int(input("Enter Legs: "))
 
 Parts = (Heads, Legs)
 Cows = int(Parts[1]/2 - Parts[0])
-Results = (Cows, abs(Parts[0] - Cows))
+Results = (abs(Cows), abs(Parts[0] - Cows))
 checkParts = ((Results[0] + Results[1]), (Results[0]*4 + Results[1]*2))
 
 if Parts == checkParts:
