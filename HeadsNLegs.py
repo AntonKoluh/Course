e435b72c344c399d8142e1a61515b557a2346f66
AntonKoluh <antonkoluh1@gmail.com>
2024-12-18 13:09:23 +0200

    Update HeadsNLegs.py

--- HeadsNLegs.py
+++ HeadsNLegs.py
@@ -2,12 +2,8 @@ Heads = int(input("Enter Heads: "))
 Legs = int(input("Enter Legs: "))
 
 Parts = (Heads, Legs)
-
 Cows = int(Parts[1]/2 - Parts[0])
-
-
 expectedParts = (Cows + Parts[0] - Cows, (Parts[0] - Cows)*2 + Cows*4)
-
 
 if (expectedParts[0] - Parts[0]) or (expectedParts[1] - Parts[1]) != 0:
     print("Impossible Solution")
