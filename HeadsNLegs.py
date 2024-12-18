e4c26512eaa18ed4ba6b10d9b69195bd6cc2fea8
AntonKoluh <antonkoluh1@gmail.com>
2024-12-18 14:06:43 +0200

    Update HeadsNLegs.py

--- HeadsNLegs.py
+++ HeadsNLegs.py
@@ -1,12 +1,12 @@
-Heads = int(input("Heads: "))
-Legs = int(input("Legs: "))
+Heads = int(input("Enter Heads: "))
+Legs = int(input("Enter Legs: "))
 
 Parts = (Heads, Legs)
 Cows = int(Parts[1]/2 - Parts[0])
 Results = (Cows, abs(Parts[0] - Cows))
-checkParts = ((Results[0] + Results[1]), (Results[0]*4 + Results[1]*2))
+checkParts = ((Results[0]+Results[1]), (Results[0]*4 + Results[1]*2))
 
-if (checkParts[0] - Parts[0]) or (checkParts[1] - Parts[1]) is not 0:
-    print ("Impossible")
-else:
+if Parts == checkParts:
     print(f"(Cow, Chicken): {Results}")
+else:
+    print ("Impossible")
